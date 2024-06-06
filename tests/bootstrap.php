44c9c0905a6d93d1030cb13ea4595fd41ca18e44
Filipe Silva <silvam.filipe@gmail.com>
2024-06-06 16:56:34 +0000

    Setup test environment

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This file is part of web-stacj
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+define('APP_ROOT', dirname(__DIR__));
